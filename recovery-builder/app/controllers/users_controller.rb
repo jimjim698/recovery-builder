115563cb0dbc6d97f1e84a35bcbbf32786df0b95
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-24 04:39:58 +0000

    created user show page

--- recovery-builder/app/controllers/users_controller.rb
+++ recovery-builder/app/controllers/users_controller.rb
@@ -5,8 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise params.inspect
-  end 
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    binding.pry
+    redirect_to user_path(@user)
+  end
 
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :manager)
+  end
 
 end
