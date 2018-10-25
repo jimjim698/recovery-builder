33cb021f1b05563c536cb61f6ff40706c096b380
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-25 19:07:50 +0000

    added signout routes and controller action

--- recovery-builder/app/controllers/sessions_controller.rb
+++ recovery-builder/app/controllers/sessions_controller.rb
@@ -1,7 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
-  end 
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+      if @user
+        @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
 
 
 end
