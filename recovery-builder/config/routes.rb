f726c1692c962852dca9159dbec9c231555a2d33
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-25 18:33:41 +0000

    set up sign in sessions page

--- recovery-builder/config/routes.rb
+++ recovery-builder/config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root 'application#home'
 
   resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
