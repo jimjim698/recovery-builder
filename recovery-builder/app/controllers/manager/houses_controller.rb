8f87e0186916485ca6c7b438de21f98a7c08ef7e
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-27 18:22:43 +0000

    added private method to house controller

--- recovery-builder/app/controllers/manager/houses_controller.rb
+++ recovery-builder/app/controllers/manager/houses_controller.rb
@@ -1,6 +1,14 @@
 class Manager::HousesController <ApplicationController
-
+    before_action :is_manager 
   def new
-  end 
+  end
 
+
+
+
+  private
+
+  def is_manager
+    current_user.manager
+  end
 end
--- recovery-builder/app/models/user.rb
+++ recovery-builder/app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   validates :name, :presence => true
-  validates  :name, :uniqueness => true 
+  validates  :name, :uniqueness => true
   has_secure_password
   belongs_to :house
   has_many :completions
   has_many :goals, through: :completions
+
+  def current_user
+    @user = User.find(params[:user_id])
+  end 
 
 end
