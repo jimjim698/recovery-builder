27e04e9cdbd34a540c09eef96cd604abe75dc975
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-27 18:58:43 +0000

    added name column to house

--- recovery-builder/db/schema.rb
+++ recovery-builder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023221002) do
+ActiveRecord::Schema.define(version: 20181027185002) do
 
   create_table "completions", force: :cascade do |t|
     t.integer  "goal_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20181023221002) do
   create_table "houses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
