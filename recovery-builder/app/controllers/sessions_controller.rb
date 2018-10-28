df7ffeb76a5ad8440ec96eaa666a0196a8887eb8
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-28 02:53:45 +0000

    sessions controller omniauth

--- recovery-builder/app/controllers/sessions_controller.rb
+++ recovery-builder/app/controllers/sessions_controller.rb
@@ -5,6 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:provider]
+      @user = User.find_or_create_by(:provider=> auth[:provider], :uid=> auth[:uid]) do |user|
+        @user.name = auth[:info][:name]
+        session[:user_id] = @user.id
+      end 
+    else
     @user = User.find_by(name: params[:name])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
@@ -12,12 +18,19 @@ class SessionsController < ApplicationController
     else
       redirect_to login_path
     end
+    redirect_to user_path(@user)
   end
+end 
 
   def destroy
     session.delete :user_id
     redirect_to '/'
   end
+
+  private 
+  def auth 
+    request.env["omniauth.auth"]
+  end 
 
 
 end
