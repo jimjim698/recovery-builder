74ec26c9451f36087d370a596a59fb04bf713912
jimjim698 <jimlotruglio@yahoo.com>
2018-10-30 22:36:26 -0600

    added link for viewing highlights

--- recovery-builder/app/helpers/houses_helper.rb
+++ recovery-builder/app/helpers/houses_helper.rb
@@ -1,16 +1,4 @@
 module HousesHelper
 
-#def display_residents(house)
-  #house.users.each do |u|
-#  u.name
-  #end
-#end
-
-def display_residents(house)
-  house.users.each do |u|
-  "<p><h5> #{link_to u.name, user_path(u)} has #{u.points} points</h5></p>"
-  end
-end
-
 
 end
