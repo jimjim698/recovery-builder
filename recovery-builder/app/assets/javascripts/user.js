3970485671398212e38a629636dec72466a3ae92
Jim <jimlotruglio@yahoo.com>
2019-01-06 01:09:52 -0700

    started moving req one to bio page, will not stay on next resident

--- recovery-builder/app/assets/javascripts/user.js
+++ recovery-builder/app/assets/javascripts/user.js
@@ -6,12 +6,14 @@ attachListeners()
 
 function attachListeners(){
 
-  $('#next').on('click', function(){
+  $('.next').on('click', function(e){
     console.log(this)
-    var nextId = parseInt($('#next').attr("data-id")) + 1;
+    var nextId = parseInt($('.next').attr("data-id")) + 1;
     $.get("/users/" + nextId + ".json", function(data){
       $("#username").text(data["name"])
       console.log(data["name"])
+
   })
+  
 })
 }
