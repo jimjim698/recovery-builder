7f2859f607bd451f63c9b8a083d62ac98e1e5e60
Jim <jimlotruglio@yahoo.com>
2019-01-06 21:32:15 -0700

    finished ajax to add index of mentors to mentors page

--- recovery-builder/app/assets/javascripts/user.js
+++ recovery-builder/app/assets/javascripts/user.js
@@ -18,9 +18,12 @@ function attachListeners(){
 })
 
 $('.mentors').on('click', function(e){
-  
+  $("#mentors").empty()
   $.getJSON(this.href, function(data){
-    console.log(data)
+
+    data.forEach(function(user){
+      $('#mentors').append('<p><h3>' + user["name"] + ' - ' + user["house"]["name"] + '<h3></p>')
+    })
   })
   e.preventDefault()
 })
